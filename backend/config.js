6062dc7b080bb69bc56d10725e898e77589baadd
thowfeeksalim002 <thowfeek.s@pillsbee.com>
2024-08-14 13:00:30 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  // 'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+  "mongodb+srv://root:root@cluster0.0sqlfya.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
